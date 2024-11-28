b7d1d0f06174ebe885322cf454b0eeac8d37d64c
Neno <nevenkolar@gmail.com>
2024-11-28 13:17:28 +0100

    implement material ui cache

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { trpc } from "@/utils/trpc"
-
 import type { AppProps } from "next/app"
+import CssBaseline from "@mui/material/CssBaseline"
+import { AppCacheProvider } from "@mui/material-nextjs/v15-pagesRouter"
 
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App(props: AppProps) {
+  const { Component, pageProps } = props
+
+  return (
+    <AppCacheProvider {...props}>
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
+    </AppCacheProvider>
+  )
 }
 
 export default trpc.withTRPC(App)
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,27 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript, DocumentContext } from "next/document"
 
-export default function Document() {
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+  documentGetInitialProps,
+} from "@mui/material-nextjs/v15-pagesRouter"
+import { DocumentProps } from "next/dist/shared/lib/utils"
+
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
+}
+
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const finalProps = await documentGetInitialProps(ctx)
+  return finalProps
 }
